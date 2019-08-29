1fddbfe618d180eac5049e484e61c48c8f095865
Yen Pei Chih <yenpeichih@gmail.com>
2019-08-29 15:02:25 +0800

    Task class edits and level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import Model_Class.Task;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,20 +13,28 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 //        System.out.println(" ");
         boolean terminate = false;
-        ArrayList<String>  toDoList = new ArrayList<>();
+        ArrayList<Task>  toDoList = new ArrayList<>();
         System.out.println("Hello I'm Duke");
         while (terminate == false) {
             Scanner echoObj = new Scanner(System.in);
+            Scanner markDone = new Scanner(System.in);
             String input = echoObj.nextLine();
             if (input.equals("bye")) {
                 terminate = true;
-            } else if (!input.equals("list")){
-                toDoList.add(input);
-                System.out.println("added: " + input);
-            } else {
-                for (String output : toDoList) {
-                    System.out.println(output);
+            } else if (input.equals("list")) {
+                for (Task output : toDoList) {
+                    System.out.println(output.getStatusIcon() + output.getDescription());
                 }
+            } else if (input.contains("done")) {
+                String[] test = input.split("\\s+");
+                int doneTask = Integer.parseInt(test[1]) - 1;
+                toDoList.get(doneTask).setDone();
+                System.out.println("Nice I have marked this task as done:");
+                System.out.println(toDoList.get(doneTask).getStatusIcon() + toDoList.get(doneTask).getDescription());
+            } else {
+                Task toAdd = new Task(input);
+                toDoList.add(toAdd);
+                System.out.println("added: " + input);
             }
         }
     }
--- src/main/java/Model_Class/Task.java
+++ src/main/java/Model_Class/Task.java
@@ -1,5 +1,23 @@
 package Model_Class;
 
 public class Task {
+    protected String description;
+    protected boolean isDone;
 
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[\u2713]" : "[\u2718]"); //return tick or X symbols
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
