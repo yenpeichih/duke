22e7df190b092d8c9c7b6b02a4a2ec5b282afacb
Yen Pei Chih <yenpeichih@gmail.com>
2019-08-29 14:22:51 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,16 +11,21 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 //        System.out.println(" ");
         boolean terminate = false;
+        ArrayList<String>  toDoList = new ArrayList<>();
         System.out.println("Hello I'm Duke");
         while (terminate == false) {
             Scanner echoObj = new Scanner(System.in);
             String input = echoObj.nextLine();
             if (input.equals("bye")) {
                 terminate = true;
+            } else if (!input.equals("list")){
+                toDoList.add(input);
+                System.out.println("added: " + input);
             } else {
-                System.out.println(input);
+                for (String output : toDoList) {
+                    System.out.println(output);
+                }
             }
         }
-        ArrayList<String>  toDoList = new ArrayList<>();
     }
 }
